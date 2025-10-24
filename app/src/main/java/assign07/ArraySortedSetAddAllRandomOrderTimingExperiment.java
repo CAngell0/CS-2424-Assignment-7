d5a16ef099db8abe7e299a8bf45c0434a72b49b2
Carson Angell <carsonjesseangell@gmail.com>
2025-10-23 18:26:17 -0600

    Added ArraySortedSetAddAllRandomOrderTimingExperiment and stubs for it

---
+++ app/src/main/java/assign07/ArraySortedSetAddAllRandomOrderTimingExperiment.java
@@ -0,0 +1,25 @@
+package assign07;
+
+import java.util.List;
+
+import timing.TimingExperiment;
+
+public class ArraySortedSetAddAllRandomOrderTimingExperiment extends TimingExperiment {
+
+    public ArraySortedSetAddAllRandomOrderTimingExperiment(String problemSizeName, List<Integer> problemSizes, int iterationCount) {
+        super(problemSizeName, problemSizes, iterationCount);
+    }
+
+    @Override
+    protected void setupExperiment(int problemSize) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setupExperiment'");
+    }
+
+    @Override
+    protected void runComputation() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'runComputation'");
+    }
+    
+}
