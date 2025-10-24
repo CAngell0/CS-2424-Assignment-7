a0c9ebf763b4a137db695d0c09e1fa6043013c77
Carson Angell <carsonjesseangell@gmail.com>
2025-10-23 18:15:02 -0600

    Set up expirement code for first timing expirement for ArraySortedSet

--- app/src/main/java/assign07/ArraySortedSetAddAllNearlySortedTimingExpirement.java
+++ app/src/main/java/assign07/ArraySortedSetAddAllNearlySortedTimingExpirement.java
@@ -1,10 +1,26 @@
 package assign07;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import timing.TimingExperiment;
 
 public class ArraySortedSetAddAllNearlySortedTimingExpirement extends TimingExperiment {
+    protected ArraySortedSet<Integer> sortedSet = new ArraySortedSet<Integer>();
+    protected List<Integer> elementsToAdd = new ArrayList<Integer>();
+
+    public static void main(String[] args) {
+        int iterationCount = 50;
+        int warmup = 30;
+        List<Integer> problemSizes = buildProblemSizes(10000, 1000, 20);
+
+        TimingExperiment experiment = new ArraySortedSetAddAllNearlySortedTimingExpirement("setSize", problemSizes, iterationCount);
+
+        experiment.warmup(warmup);
+        experiment.run();
+        experiment.print();
+        experiment.writeToCSV("timing.csv");
+    }
 
     public ArraySortedSetAddAllNearlySortedTimingExpirement(String problemSizeName, List<Integer> problemSizes, int iterationCount) {
         super(problemSizeName, problemSizes, iterationCount);
@@ -12,14 +28,22 @@ public class ArraySortedSetAddAllNearlySortedTimingExpirement extends TimingExpe
 
     @Override
     protected void setupExperiment(int problemSize) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setupExperiment'");
+        sortedSet.clear();
+        elementsToAdd.clear();
+        for (int number = 0; number < problemSize; number++) elementsToAdd.add(number);
+        
+        for (int i = 0; i < (int) (problemSize / 4); i++){
+            int index1 = (int) Math.random() * problemSize;
+            int index2 = (int) Math.random() * problemSize;
+
+            Integer temp = elementsToAdd.get(index1);
+            elementsToAdd.set(index1, elementsToAdd.get(index2));
+            elementsToAdd.set(index2, temp);
+        }
     }
 
     @Override
     protected void runComputation() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'runComputation'");
+        sortedSet.addAll(elementsToAdd);
     }
-    
 }
